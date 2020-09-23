035ded0a739f91c37085b4b2c2dbaeedb960cee9
crissr11 <43736409+crissr11@users.noreply.github.com>
2020-09-23 14:54:14 -0500

    Modified Main.java to add 4 integers

--- Adder/src/adder/Main.java
+++ Adder/src/adder/Main.java
@@ -7,11 +7,11 @@ public class Main {
             int result = addArguments(args);
             System.out.println(result);
         } catch (Exception e) {
-            System.err.println("Please provide two integers to add");
+            System.err.println("Please provide only 4 integers to add");
         }
     }
 
     private static int addArguments(String[] args) {
-        return Integer.valueOf(args[0]) + Integer.valueOf(args[1]);
+        return Integer.valueOf(args[0]) + Integer.valueOf(args[1]) + Integer.valueOf(args[3]) + Integer.valueOf(args[4]);
     }
 }
